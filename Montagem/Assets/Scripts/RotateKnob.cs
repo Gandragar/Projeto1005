3df20a8c45db876904f811919ed5c59857282c4d
Hugo Tonette <hugo.tonette@gmail.com>
2019-06-22 19:53:28 -0300

    knob se movem

--- Montagem/Assets/Scripts/RotateKnob.cs
+++ Montagem/Assets/Scripts/RotateKnob.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class RotateKnob : MonoBehaviour
 {
-	Vector3 mPrevPos = Vector3.zero;
-	Vector3 mPosDelta = Vector3.zero;
+	public bool VMov = false;
+
+	private bool _canMove = false;
+	private Vector3 mPrevPos = Vector3.zero;
+	private Vector3 mPosDelta = Vector3.zero;
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetMouseButton(0))
+		if (Input.GetMouseButton(0) && _canMove)
 		{
-			mPosDelta = Input.mousePosition - mPrevPos;
-			transform.Rotate(transform.forward, -Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.World);
+			if (!VMov)
+			{
+				mPosDelta = Input.mousePosition - mPrevPos;
+				transform.Rotate(transform.forward, -Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.World);
+			}
+			else
+			{
+				mPosDelta = Input.mousePosition - mPrevPos;
+				transform.Rotate(transform.forward, -Vector3.Dot(mPosDelta, Camera.main.transform.up), Space.World);
+			}
 		}
-
 		mPrevPos = Input.mousePosition;
 	}
+
+	private void OnMouseDown()
+	{
+		Debug.Log("down");
+		_canMove = true;
+	}
+
+	private void OnMouseUp()
+	{
+		Debug.Log("up");
+		_canMove = false;
+	}
+
 }
