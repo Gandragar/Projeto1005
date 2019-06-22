f10d4861b8fea9b056d7f48ba1926baaa037d3b7
Gandragar <43658930+Gandragar@users.noreply.github.com>
2019-06-22 18:51:06 -0300

    First commit
    Primeirão

---
+++ Montagem/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void SceneLoarder(int SceneIndex)
+    {
+      SceneManager.LoadScene (SceneIndex);
+    }
+}
---
+++ Montagem/Assets/Scripts/LuaMov.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuaMov : MonoBehaviour
+{
+    public float speed;
+    Vector3 position;
+    public float MaxPos = 7.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    position = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      position.x += Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+
+      position.x = Mathf.Clamp (position.x,-7.5f, 7.5f);
+
+      transform.position = position;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+      if (col.gameObject.tag == "Enemy Asteroide") {
+        Destroy (gameObject);
+      }
+    }
+}
