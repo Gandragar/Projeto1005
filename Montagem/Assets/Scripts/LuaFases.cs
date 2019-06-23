1ff5a775d9634a6f2e2e976683a2eba6a0831496
Hugo Tonette <hugo.tonette@gmail.com>
2019-06-22 21:39:59 -0300

    fases lua funcionando

---
+++ Montagem/Assets/Scripts/LuaFases.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LuaFases : MonoBehaviour
+{
+	public Image TargetImage;
+
+	[Tooltip("Colocar em ordem: Lua Nova > Lua Minguante > Lua Crescente > Lua Cheia")]
+	public Sprite[] PossibleImages;
+
+	public void LuaNova()
+	{
+		TargetImage.sprite = PossibleImages[0];
+	}
+
+	public void LuaMinguante()
+	{
+		TargetImage.sprite = PossibleImages[1];
+	}
+
+	public void LuaCrescente()
+	{
+		TargetImage.sprite = PossibleImages[2];
+	}
+
+	public void LuaCheia()
+	{
+		TargetImage.sprite = PossibleImages[3];
+	}
+}
